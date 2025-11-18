b2a09b3ee562105d2d053f16a667c78a7e6942bf
Cristian Moreno <benavidesmoreno1@gmail.com>
2025-11-18 17:24:09 +0100

    chart

---
+++ frontend/src/components/ChartCard.jsx
@@ -0,0 +1,15 @@
+// frontend/src/components/ChartCard.jsx
+import React from "react";
+
+const ChartCard = ({ title, children }) => {
+  return (
+    <div className="chart-card">
+      <div className="chart-card-header">
+        <h3>{title}</h3>
+      </div>
+      <div className="chart-card-body">{children}</div>
+    </div>
+  );
+};
+
+export default ChartCard;
